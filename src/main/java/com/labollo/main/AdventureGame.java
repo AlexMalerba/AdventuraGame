f608c612117cc1d4c6723e7118b36b691dba7b79
labollo <alexmalerba0@gmail.com>
2023-11-20 19:38:12 +0100

    Codice aggiornato

--- src/main/java/com/labollo/main/AdventureGame.java
+++ src/main/java/com/labollo/main/AdventureGame.java
@@ -23,19 +23,19 @@ public class AdventureGame {
 
             // Configuring the window layout using a CardLayout
             window.setLayout(new CardLayout()); // Sets a CardLayout as the layout manager to handle panels in the window
-            window.add(menuPanel);
-            window.add(gamePanel);
-            window.pack();
+            window.add(menuPanel); // Adds the menu panel to the window.
+            window.add(gamePanel); // Adds the game panel to the window.
+            window.pack(); // Sizes the window to fit its preferred size, taking into account the added panels.
 
-            gamePanel.setupGame();
-            gamePanel.startGameThread();
+            gamePanel.setupGame(); // Initializes the game settings and components.
+            gamePanel.startGameThread(); // Starts the game thread responsible for continuous updates and rendering.
 
             // Configuring window dimensions and position
             window.setSize(gamePanel.screenWidth, gamePanel.screenHeight); // Sets the window dimensions: 768x576
             window.setLocationRelativeTo(null); // Sets the window to the center of the screen
             window.setVisible(true); // Sets the "window" object visible
-            // Initially shows the game panel
-            ((CardLayout) window.getContentPane().getLayout()).show(window.getContentPane(), "game");
+
+            ((CardLayout) window.getContentPane().getLayout()).show(window.getContentPane(), "game"); // Initially shows the game panel
         });
     }
 }
